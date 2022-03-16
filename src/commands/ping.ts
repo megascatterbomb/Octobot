ec7c5b6e12cf6c266222d406592e6888bf0b7fe2
megascatterbomb <dachingygang@gmail.com>
2022-03-17 12:23:37 +1300

    Ping command no longer restricted to admins

--- src/commands/ping.ts
+++ src/commands/ping.ts
@@ -14,7 +14,7 @@ import { Message } from "discord.js";
 
 @Alias("ping", "hello", "test")
 @Inhibit({ limitBy: "USER", maxUsesPerPeriod: 3, periodDuration: 10 })
-@Permit("ADMINISTRATOR")
+
 export default class PingCommand extends Command {
 
     async execute(message: Message, client: Client) {
