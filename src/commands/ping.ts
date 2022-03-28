6b1f5bff95cb82a0356ccdf2a9391c70c2f08ddf
megascatterbomb <dachingygang@gmail.com>
2022-03-29 12:01:17 +1300

    Uptime command

--- src/commands/ping.ts
+++ src/commands/ping.ts
@@ -21,6 +21,6 @@ export default class PingCommand extends ChannelCommand {
 
     async execute(message: Message, client: Client) {
         console.log("Ping received from " + message.author.username + " " + message.author.id); 
-        message.channel.send("Pong!");
+        message.channel.send("Pong! Uptime: " + (process.uptime()/3600).toFixed(2) + " hours");
     }
 }
