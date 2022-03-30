b02495db4fc9b2b3c8ffe064e20adf1783dec844
megascatterbomb <dachingygang@gmail.com>
2022-03-30 16:02:27 +1300

    Moved uptime to its own command

--- src/commands/uptime.ts
+++ src/commands/uptime.ts
@@ -14,13 +14,12 @@ import {
 import { Message } from "discord.js";
 import ChannelCommand from "../extensions/channelCommand";
 
-@Alias("ping", "hello", "test")
+@Alias("uptime")
 @Inhibit({ limitBy: "USER", maxUsesPerPeriod: 3, periodDuration: 10 })
 @Described("Test if the bot is alive")
-export default class PingCommand extends ChannelCommand {
+export default class UptimeCommand extends ChannelCommand {
 
     async execute(message: Message, client: Client) {
-        console.log("Ping received from " + message.author.username + " " + message.author.id); 
-        message.channel.send("Pong!");
+        message.channel.send("Uptime: " + (process.uptime()/3600).toFixed(2) + " hours");
     }
 }
