6327e7b8a813e1b99c9233c97328879d43f2f31c
megascatterbomb <dachingygang@gmail.com>
2022-04-06 16:53:35 +1200

    Added average balance to $stats

--- src/commands/stats.ts
+++ src/commands/stats.ts
@@ -26,10 +26,11 @@ export default class StatsCommand extends ChannelCommand {
         const totalOctobucks = balances.reduce((accumulator, object) => {
             return accumulator + object.balance;
         }, 0);
+        const averageBalance = totalOctobucks/balances.length;
         const rateUSDToOcto = totalOctobucks/totalUSDInvested;
         const rateOctoToUSD = totalUSDInvested/totalOctobucks;
 
-        message.reply("```\nTotal Octobucks: $" + totalOctobucks + "\nTotal USD Invested: $" + 
+        message.reply("```\nTotal Octobucks: $" + totalOctobucks + "\nAverage Balance: $" + averageBalance.toFixed(2) + "\nTotal USD Invested: $" + 
             totalUSDInvested + "\n1 USD = " + rateUSDToOcto.toPrecision(5) + " Octobucks\n1 Octobuck = " + rateOctoToUSD.toPrecision(5) + " USD```");
     }
 }
