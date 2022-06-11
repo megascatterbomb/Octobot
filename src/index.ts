54268201eb47bdcbcb2451d22577507ae8635e37
megascatterbomb <dachingygang@gmail.com>
2022-06-12 11:45:47 +1200

    Added retry capability to handle 500 errors

--- src/index.ts
+++ src/index.ts
@@ -12,7 +12,8 @@ require("dotenv").config();
 export let client: Client;
 
 (async () => {
-    client = new Client({defaultCommandPrefix: "$", owners: ["193950601271443456"], intents: [Intents.resolve(32767)]});
+    client = new Client({defaultCommandPrefix: "$", owners: ["193950601271443456"], intents: [Intents.resolve(32767)], 
+        restRequestTimeout: 30000, retryLimit: 5, });
     await client.registry.recursivelyRegisterCommands(path.join(__dirname, "/commands"));
     await client.registry.recursivelyRegisterEvents(path.join(__dirname, "/events"));
 
@@ -24,6 +25,7 @@ export let client: Client;
 
 async function onReady() {
     console.log("Connected to Discord");
+    client.user?.setPresence({status: "online"});
     const channel = client.channels.resolve(allowedChannels[0]) as TextChannel | undefined;
     channel?.send("Octobot launched successfully.");
 
